6053efc7051b7d0ef8bf9aa6e5c404b8e6c9769b
Amit <amit.silicon2008@gmail.com>
2023-07-13 23:29:55 +0530

    Add more training data

--- pkg/training-template.go
+++ pkg/training-template.go
@@ -12,5 +12,20 @@ Output: ls -a
 
 Input: Count files in a directory
 Output: ls -a | wc -l
+
+Input: Disk space used by home directory
+Output: du -sh ~
+
+Input: Replace foo with bar in all .go files
+Output: find . -name "*.go" | xargs sed -i 's/foo/bar/g'
+
+Input: Delete the test subdirectory and everything underneath it
+Output: rm -rf test
+
+Input: Splunk query for all 500 errors
+Output: index=<app_index> status=500
+
+Input: Splunk query to find out all successful logs
+Output: index=<app_index> <your_text_search_query> status=200
 `
 )
